96185a080e464a3df5f2b7fc62181f3d4bc19b27
Alan-R-Silva <alandasilvarisson@gmail.com>
2022-07-15 22:15:39 -0300

    menu-buttons

--- script/navTop.js
+++ script/navTop.js
@@ -1,5 +1,8 @@
 const contact = document.querySelector('.contact')
+const about = document.querySelector('.aboutMe')
 let showContact = true
+let showAbout = true
+
 const clickMenu = (link) => {
 
     switch (link){
@@ -8,6 +11,35 @@ const clickMenu = (link) => {
 
             contact.style.display = showContact ? 'flex' : 'none'
             showContact = !showContact
+        break;
+
+        case 'about':
+
+            contact.style.display = 'none'
+            showContact = true
+            
+            document.body.style.overflow = showAbout? 'hidden' : 'initial'
+            about.style.display = showAbout? 'flex' : 'none'
+            showAbout = !showAbout
+            if(showAbout) {
+                container.classList.remove('roll')
+            } else {
+                container.classList.add('roll')
+            }
+
+        break;
+
+        case 'home':
+
+            contact.style.display = 'none'
+            showContact = true
+            about.style.display = 'none'
+            showAbout = true
+            if (window.scrollY < 10) {
+                container.classList.remove('roll')
+            }
+            document.body.style.overflow = 'initial'
+
         break;
 
     }
